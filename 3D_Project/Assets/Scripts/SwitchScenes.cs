bd7b7e6d373fc8356bfa490d344e1054f4451b44
loreleibasken <lbasken@student.dwc.edu>
2017-03-03 22:49:21 -0800

    Added SwitchScenes scripts, buttons work when clicked

---
+++ 3D_Project/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScenes : MonoBehaviour {
+    // attached to each button to switch between scenes
+    // note: do not swap Application for SceneManager
+	    public void loadGame()
+    {
+        Application.LoadLevel("Lorelei's GameScene");
+    }
+
+    public void endGame()
+    {
+        Application.LoadLevel("GameOverScreen");
+    }
+
+    public void startGame()
+    {
+        Application.LoadLevel("StartScreen");
+    } 
+}
