ce37d72a608b8839e7f6b9842fd88e95b0f04f28
loreleibasken <lbasken@student.dwc.edu>
2017-03-07 21:14:15 -0800

    brought vase to Lorelei's scene

---
+++ 3D_Project/Assets/Scripts/ChangeDirection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeDirection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /*
+    void OnCollisionEnter(Collision c)
+    {
+        // force is how forcefully we will push the player away from the enemy.
+        float force = 3;
+
+        // If the object we hit is the enemy
+        if (c.gameObject.tag == "Vase")
+        {
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = c.contacts[0].point - transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir * force);
+        }
+    }
+    */
+
+    public void OnCollisionEnter(Collision other)
+    {
+        // how much the character should be knocked back
+        var magnitude = 5000;
+        // calculate force vector
+        var force = transform.position - other.transform.position;
+        // normalize force vector to get direction only and trim magnitude
+        force.Normalize();
+        gameObject.GetComponent<Rigidbody>().AddForce(force * magnitude);
+    }
+}
---
+++ 3D_Project/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+    
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
