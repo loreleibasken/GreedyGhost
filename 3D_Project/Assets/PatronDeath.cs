a511e2227277571c22d45224683a28f2d280b71e
sdenoncourt <sdenoncourt@ERC117-14>
2017-03-07 23:21:29 -0500

    added my stuff

---
+++ 3D_Project/Assets/PatronDeath.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatronDeath : MonoBehaviour {
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "NPC")
+        {
+            GameObject.Find("HUD").GetComponent<GameController>().ded += 1;
+            
+            Destroy(other.gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
